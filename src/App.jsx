68c910db8f2017e7126313cbde83203bcb547e45
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-03 18:54:53 +0500

    add search setion & categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
       <Navbar/>
 
       </div>

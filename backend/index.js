b09c28248143f2b4677b00d051302e52bca8e399
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-06 13:37:12 +0500

    upadate these files

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express"
 import router from "./routes/user.route.js"
 import postrouter from "./routes/post.route.js"
 import commentrouter from "./routes/comment.route.js"
+import connectDB from "./lib/connectDB.js"
+import dotenv from 'dotenv';
+dotenv.config();  
+
 const app = express()
 
 // app.get("/test",(req,res)=>{
@@ -15,6 +19,7 @@ app.use("/comment",commentrouter)
 // console.log("changing.");
 
 app.listen(8000,()=>{
+    connectDB()
     console.log("server is runing");
     
 })

4ca63a58ee67e1424cb6fd06367c8bf44634ebc8
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-02-07 16:48:53 +0500

    authentication/protecting routes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,6 @@
 import postModel from "../models/post.model.js";
+import User from "../models/post.model.js";
+
 
 export const getPosts = async(req,res) =>{
     const posts = await postModel.find();
@@ -12,14 +14,23 @@ export const getPost = async(req,res)=>{
 }
 
 export const createPost = async(req,res)=>{
-    const newPost = new postModel(req.body);
+    const clerkUserId = req.auth.userId;
+    if(!clerkUserId){
+        return res.status(401).json("Not authenticated")
+    }
 
+    const user = await User.findOne({clerkUserId});
+
+    if(!user){
+        return res.status(404).json("user not found")
+    }
+    const newPost = new postModel({user:user._id,...req.body});
     const post = await newPost.save();
     res.status(200).json(post)
 }
 
 export const deletePost = async(req,res)=>{
    
-    const post = await postModel.findByIdAndDelete(req.params.id);
+    const post = await postModel.findByIdAndDelete({_id:req.params.id,user:user._id});
     res.status(200).json(" has been deleted")
 }
--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,14 @@ import router from "./routes/user.route.js"
 import postrouter from "./routes/post.route.js"
 import commentrouter from "./routes/comment.route.js"
 import webhookrouter from "./routes/webhook.route.js"
-
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 import connectDB from "./lib/connectDB.js"
 import dotenv from 'dotenv';
 dotenv.config();  
 
 const app = express()
 app.use("/webhooks",webhookrouter)
+app.use(clerkMiddleware());
 app.use(express.json())
 app.use((error,req,res,next)=>{
     res.status(error.status || 500);
@@ -24,6 +25,25 @@ app.use((error,req,res,next)=>{
 // app.get("/test",(req,res)=>{
 //     res.status(200).send("it is working")
 // })
+
+// app.get("/auth-state",(req,res)=>{
+//     const authState = req.auth;
+//     res.json(authState);
+// })
+
+
+// app.get("/protect",(req,res)=>{
+//     const {userid} = req.auth;
+//     if(!userid){
+//         return res.status(401).json("not authenticated")
+//     }
+//     res.status(200).json("content");
+// })
+
+app.get("/protect2",requireAuth(),(req,res)=>{
+       
+    res.status(200).json("content");
+})
 
 app.use("/users",router)
 app.use("/posts",postrouter);
